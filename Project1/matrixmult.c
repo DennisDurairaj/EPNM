445d083aabfa8d77e561c4344a9ea60dbd6976d7
Dennis Durairaj <dennis.durairaj@gmail.com>
2016-03-24 22:29:58 +0100

    Cleaned up code

--- Project1/matrixmult.c
+++ Project1/matrixmult.c
@@ -1,17 +1,23 @@
 #include <mpi.h>
 #include <stdio.h>
 
+#define SIZE 8
+
+void inputData(int* nrows, int* ncols) {
+	int rows;
+	printf("%s\n", "Enter number of rows and columns for the matrix");
+	scanf("%d", rows);
+	*nrows = rows;
+	*ncols = rows;
+}
+
 int fillMatrix(int nrows, int ncols, int mat[nrows][ncols]) {
 	// printf("%s\n", "Matrix");
 	int count = 1;
-	for (int i = 0; i < nrows; i++)
-	{
-		for (int j = 0; j < ncols; j++)
-		{
+	for (int i = 0; i < nrows; i++) {
+		for (int j = 0; j < ncols; j++) {
 			mat[i][j] = count++;
-			// printf("%d ",mat[i][j]);
 		}
-	// printf("\n");
 	}
 	return mat[nrows][ncols];
 }
@@ -19,20 +25,15 @@ int fillMatrix(int nrows, int ncols, int mat[nrows][ncols]) {
 int fillVector(int nrows, int vec[nrows]) {
 	int count = 1;
 	// printf("%s\n","Vector");
-	for (int i = 0; i < nrows; i++)
-	{
+	for (int i = 0; i < nrows; i++) {
 		vec[i] = count++;
-		// printf("%d ",vec[i]);
 	}
-	// 	printf("\n");
 	return vec[nrows];
 }
 
 void printMatrix(int nrows, int ncols, int mat[nrows][ncols]) {
-	for (int i = 0; i < nrows; ++i)
-	{
-		for (int j = 0; j < ncols; ++j)
-		{
+	for (int i = 0; i < nrows; ++i) {
+		for (int j = 0; j < ncols; ++j) {
 			printf("%d ",mat[i][j]);
 		}
 		printf("\n");
@@ -40,34 +41,27 @@ void printMatrix(int nrows, int ncols, int mat[nrows][ncols]) {
 }
 
 void printVector(int nrows, int vec[nrows]) {
-	for (int i = 0; i < nrows; ++i)
-	{
+	for (int i = 0; i < nrows; ++i) {
 		printf("%d ", vec[i]);
 	}
 	printf("\n");
 }
 
 void calcMult(int nrows, int ncols, int mat[nrows][ncols], int vec[nrows], int result[nrows], int begin, int end) {
-	for (int i = begin; i < end; ++i)
-	{
-		for (int j = 0; j < nrows; ++j)
-		{
+	for (int i = begin; i < end; ++i) {
+		for (int j = 0; j < nrows; ++j) {
 			result[i] += mat[i][j]*vec[j];
 		}
 	}
 }
 
 int main(int argc, char *argv[]) {
-	int nrows = 8;
-	int ncols = 8;
-	int result[8] = {0};
-	int mult[8] = {0};
+	int nrows = SIZE;
+	int ncols = SIZE;
+	int result[SIZE] = {0};
+	int mult[SIZE] = {0};
 	int mat[nrows][ncols];
 	int vec[nrows];
-	// int tag = 666;
-	// mat[8][8] = fillMatrix(nrows, ncols, mat);
-	// vec[8] = fillVector(ncols, vec);
-	// calcMult(mat, vec, mult);
 
 	int myid, nprocs;
 	MPI_Init (&argc, &argv);
@@ -79,55 +73,19 @@ int main(int argc, char *argv[]) {
 	if (myid == 0) {
 		mat[nrows][ncols] = fillMatrix(nrows, ncols, mat);
 		vec[nrows] = fillVector(nrows, vec);
-		// MPI_Send(mat, 8*8/nprocs, MPI_INT, 1, tag, MPI_COMM_WORLD);
-		// for (int i = begin; i < end; ++i)
-		// {
-		// 	for (int j = 0; j < 8; ++j)
-		// 	{
-		// 		result[i] += mat[i][j]*vec[j];
-		// 	}
-		// }
-		// MPI_Recv(result, 8/nprocs, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
-
-	// if (myid == 1) {
-	// 	MPI_Recv(mat, 8*8/nprocs, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	// 	for (int i = begin; i < end; ++i)
-	// 	{
-	// 		for (int j = 0; j < 8; ++j)
-	// 		{
-	// 			result[i] += mat[i][j]*vec[j];
-	// 		}
-	// 	}
-	// 	MPI_Send(result, 8/nprocs, MPI_INT, 0, tag, MPI_COMM_WORLD);
-	// }
 
 	MPI_Bcast(vec, nrows, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Scatter(mat, nrows*ncols/nprocs, MPI_INT, mat[begin], nrows*ncols/nprocs, MPI_INT, 0, MPI_COMM_WORLD);
 
 	calcMult(nrows, ncols, mat, vec, result, begin, end);
-	// for (int i = begin; i < end; i++) {
-	// 	for (int j = 0; j < 8; j++) {
-	// 		printf("%d ",mat[i][j] );
-	// 	}
-	// }
 
 	MPI_Gather(&result[begin], nrows/nprocs, MPI_INT, &mult[begin], nrows/nprocs, MPI_INT, 0, MPI_COMM_WORLD);
 
-	if (myid == 0)
-	{
+	if (myid == 0) {
 		printVector(nrows, vec);
 		printVector(nrows, mult);
-
 	}
-		// printf("\n\n");
-		// int count = 0;
-		// for (int i=0; i < 8; i++) {
-		// 	printf("%d ", result[i]);
-		// 	count++;
-		// }
-		// printf("\n%d\n", count);
-		// printMatrix(mat);
 
 	MPI_Finalize();
 	return 0;
